3260b3100159bf841e80f6f5bdb535e98e4a0b36
fecrakib <rakibislam@132575gmial.com>
2022-12-28 12:02:17 +0600

    different types of variable

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
 // console.log('wellcome')
-let porcoutry=['Bangladess','India','Pakistan'];
-let richcountry=[...porcoutry,'America','Canada'];
-console.log(richcountry);
-// add array without spread operator.
-let num1=[2,3,4,5,6];
-let num2=[10,20,30,40];
-num1.push(num2);
-console.log(num1);
-// Rest parameter (Rest parameter is prefixed with three dots like(...))
-function show(...numbers){
-let sum=0;
-for(let i of numbers){
-    sum=sum+i;
-}
-console.log("summation="+sum);
-}
-show(10,20,30,40);
+// let porcoutry=['Bangladess','India','Pakistan'];
+// let richcountry=[...porcoutry,'America','Canada'];
+// console.log(richcountry);
+// // add array without spread operator.
+// let num1=[2,3,4,5,6];
+// let num2=[10,20,30,40];
+// num1.push(num2);
+// console.log(num1);
+// // Rest parameter (Rest parameter is prefixed with three dots like(...))
+// function show(...numbers){
+// let sum=0;
+// for(let i of numbers){
+//     sum=sum+i;
+// }
+// console.log("summation="+sum);
+// }
+// show(10,20,30,40);
+// // dymanic function (function are assained in a variable)
+// var name=function(nameValue){
+//     return nameValue;
+// }
+// console.log(name("Rakib"));
+//  differnt method of declare variable
+// 1. using let
+// i)let keyword is assigned the block scope 
+// ii) let cannot be re-declared.
+// iii) can be reassinged a value
+
+
+let name="Rakib";
+name="Rahim";//Reasing...
+console.log(name);
+// let name="Takvir";// Re-declarealbe is not possible
+
+
+
+
+
+// using const:-
+// i)it connot be re-assigned a value
+// ii)it is block scope
+// iii)A constant cannot be re-declared
+const a=5;
+// a=110;// Re-assin is not possible.
+console.log(a);
+
+
+// using var:-
+// let can be re-declared 
+// can be reassigned a value
+var nam="Rakib";
+nam="Karim";//Reasing...
+var nam= "Jadid";//Re-declareable.
+console.log(nam);
+
